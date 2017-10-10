3db667993549272098cf57c70ab292b2352c9fe5
tehlordvortex <hadokoda@yahoo.com>
2017-10-10 14:01:35 +0100

    fix indentation in StartPage.vue
    also remove empty CSS declaration.

--- src/components/StartPage.vue
+++ src/components/StartPage.vue
@@ -1,21 +1,21 @@
 <template>
     <div id="content">
-        <v-layout row>
-            <v-fab-transition>
-                <v-flex v-if="ready && noTodos" xs12 sm6 offset-sm3>
-                    <v-card class="primary white">
-                        <v-card-title primary-title><h3 class="headline mb-0">SO BLENK</h3></v-card-title>
-                        <v-card-text>You have yet to create any ToDo, Ser User.</v-card-text>
-                        <v-card-actions>
-                            <v-btn flat to="/create/todo" class="blue--text">Create</v-btn>
-                        </v-card-actions>
-                    </v-card>
-                </v-flex>
-            </v-fab-transition>
-            <v-flex  v-if="!ready" offset-sm6>
-              <v-progress-circular indeterminate class="primary--text"></v-progress-circular>
-            </v-flex>
-        </v-layout>
+      <v-layout row>
+        <v-fab-transition>
+          <v-flex v-if="ready && noTodos" xs12 sm6 offset-sm3>
+            <v-card class="primary white">
+              <v-card-title primary-title><h3 class="headline mb-0">SO BLENK</h3></v-card-title>
+              <v-card-text>You have yet to create any ToDo, Ser User.</v-card-text>
+              <v-card-actions>
+                <v-btn flat to="/create/todo" class="blue--text">Create</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-flex>
+        </v-fab-transition>
+        <v-flex  v-if="!ready" offset-sm6>
+          <v-progress-circular indeterminate class="primary--text"></v-progress-circular>
+        </v-flex>
+      </v-layout>
     </div>
 </template>
 
@@ -23,28 +23,28 @@
 export default {
     name: 'startPage',
     created() {
-        if(this.$root.$data.database.isReady()) {
-            this.noTodos = this.$root.$data.database.getTodos().count() < 1
-            if (this.noTodos) {
-                this.ready = true
-                return
-            }
-            this.$router.push({name: 'todos'})
+      if(this.$root.$data.database.isReady()) {
+        this.noTodos = this.$root.$data.database.getTodos().count() < 1
+        if (this.noTodos) {
+          this.ready = true
+          return
         }
-        else this.$root.$data.database.whenReady(() => {
-            this.noTodos = this.$root.$data.database.getTodos().count() < 1
-            if (this.noTodos) {
-                this.ready = true
-                return
-            }
-            this.$router.push({name: 'todos'})
-        })
+        this.$router.push({name: 'todos'})
+      }
+      else this.$root.$data.database.whenReady(() => {
+        this.noTodos = this.$root.$data.database.getTodos().count() < 1
+        if (this.noTodos) {
+          this.ready = true
+          return
+        }
+        this.$router.push({name: 'todos'})
+      })
     },
     data() {
-        return {
-            ready: false,
-            noTodos: false,
-        }
+      return {
+        ready: false,
+        noTodos: false,
+      }
     },
     methods: {
     }
@@ -52,7 +52,4 @@ export default {
 </script>
 
 <style scoped>
-#content {
-
-}
 </style>
