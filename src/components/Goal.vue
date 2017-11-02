0158ee170553b99dc6fdd074e8a9914f02be5644
tehlordvortex <hadokoda@yahoo.com>
2017-11-02 12:33:33 +0100

    add error handling to Goal.vue

--- src/components/Goal.vue
+++ src/components/Goal.vue
@@ -1,7 +1,7 @@
 <template>
   <v-layout>
     <v-flex xs12 sm10 offset-sm1>
-      <v-card>
+      <v-card v-if="!this.notFound">
         <v-card-title>
           <div class="headline">{{ goal.name }}</div>
         </v-card-title>
@@ -12,6 +12,15 @@
            </span>
           <ToDos v-bind:goal="goal"></ToDos>
         </v-card-text>
+      </v-card>
+      <v-card v-if="this.notFound">
+        <v-card-title>
+          <div class="headline">Goal Not Found</div>
+        </v-card-title>
+        <v-card-text>There exists no goal with the specified ID</v-card-text>
+        <v-card-actions>
+          <v-btn flat primary @click.native.stop="goBack()">Back</v-btn>
+        </v-card-actions>
       </v-card>
     </v-flex>
   </v-layout>
@@ -26,12 +35,18 @@ export default {
   },
   data() {
     return {
-      goal: null
+      goal: null,
+      notFound: false
     }
   },
   methods: {
     buildData(db) {
       this.goal = db.getGoal(this.$route.params.id)
+      if (!this.goal)
+        this.notFound = true
+    },
+    goBack() {
+      this.$router.go(-1)
     }
   },
   components: {
