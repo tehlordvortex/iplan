4ff0510909146708873aee4b02b3ffb81fe26e84
tehlordvortex <hadokoda@yahoo.com>
2017-11-08 13:16:01 +0100

    add confirmation dialog to Goals.vue & Goal.vue
    delete todos attached to a goal when the goal is deleted

--- src/components/Goal.vue
+++ src/components/Goal.vue
@@ -4,6 +4,9 @@
       <v-card v-if="!this.notFound">
         <v-card-title>
           <div class="headline">{{ goal.name }}</div>
+          <div>
+            <v-btn icon flat @click.native.stop="deleteGoal"><v-icon>delete</v-icon></v-btn>
+          </div>
         </v-card-title>
         <v-card-text>
            <span v-show="goal.dueDate">
@@ -22,6 +25,21 @@
           <v-btn flat primary @click.native.stop="goBack()">Back</v-btn>
         </v-card-actions>
       </v-card>
+      <v-dialog
+          persistent
+          v-model="showDeleteConfirm"
+          lazy
+          full-width
+      >
+        <v-card>
+          <v-card-title><span class="headline mb-0">Are you sure?</span></v-card-title>
+          <v-card-text>Are you sure you want to delete these goals(s)? This action is irreversible.</v-card-text>
+          <v-card-actions>
+            <v-btn @click.native.stop="confirmCallback" flat class="blue--text">Yes</v-btn>
+            <v-btn @click.native.stop="abortCallback" flat class="red--text">No</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
     </v-flex>
   </v-layout>
 </template>
@@ -36,7 +54,8 @@ export default {
   data() {
     return {
       goal: null,
-      notFound: false
+      notFound: false,
+      showDeleteConfirm: false
     }
   },
   methods: {
@@ -44,6 +63,23 @@ export default {
       this.goal = db.getGoal(this.$route.params.id)
       if (!this.goal)
         this.notFound = true
+    },
+    deleteGoal() {
+      this.showDeleteConfirm = true
+      this.confirmCallback = () => {
+        this.goal.todo_ids.forEach((todoId) => {
+          var todo = this.$root.$data.database.getToDo(todoId)
+          if (todo.goalId && todo.goalId == this.goal._id) {
+            this.$root.$data.database.deleteToDo(todo)
+          }
+        })
+        this.$root.$data.database.deleteGoal(this.goal)
+        this.showDeleteConfirm = false
+        this.$router.go(-1)
+      }
+      this.abortCallback = () => {
+        this.showDeleteConfirm = false
+      }
     },
     goBack() {
       this.$router.go(-1)
--- src/components/Goals.vue
+++ src/components/Goals.vue
@@ -40,6 +40,21 @@
           <v-btn to="/todos" class="blue--text" flat>Create One</v-btn>
         </v-card-actions>
       </v-card>
+      <v-dialog
+          persistent
+          v-model="showDeleteConfirm"
+          lazy
+          full-width
+      >
+        <v-card>
+          <v-card-title><span class="headline mb-0">Are you sure?</span></v-card-title>
+          <v-card-text>Are you sure you want to delete these goals(s)? This action is irreversible.</v-card-text>
+          <v-card-actions>
+            <v-btn @click.native.stop="confirmCallback" flat class="blue--text">Yes</v-btn>
+            <v-btn @click.native.stop="abortCallback" flat class="red--text">No</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
     </v-flex>
   </v-layout>
 </template>
@@ -60,7 +75,8 @@ export default {
   },
   data() {
     return {
-      goals: []
+      goals: [],
+      showDeleteConfirm: false
     }
   },
   methods: {
@@ -68,8 +84,21 @@ export default {
       this.$router.push({name: 'goal', params: {id: id}})
     },
     deleteGoal(goal) {
-      this.goals = this.goals.filter((g) => g != goal)
-      this.$root.$data.database.deleteGoal(goal)
+      this.showDeleteConfirm = true
+      this.confirmCallback = () => {
+        this.goals = this.goals.filter((g) => g != goal)
+        goal.todo_ids.forEach((todoId) => {
+          var todo = this.$root.$data.database.getToDo(todoId)
+          if (todo.goalId && todo.goalId == goal._id) {
+            this.$root.$data.database.deleteToDo(todo)
+          }
+        })
+        this.$root.$data.database.deleteGoal(goal)
+        this.showDeleteConfirm = false
+      }
+      this.abortCallback = () => {
+        this.showDeleteConfirm = false
+      }
     }
   },
   components: {
