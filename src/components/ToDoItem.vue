239bc444f0ec38ac74e39c62522552794375f994
tehlordvortex <hadokoda@yahoo.com>
2018-02-08 14:24:46 +0100

    fix selection bug on small screens
    selecting an item on small screens would also open the actions tile. that no longer happens.

--- src/components/ToDoItem.vue
+++ src/components/ToDoItem.vue
@@ -1,51 +1,53 @@
 <template>
   <div>
     <div class="hidden-sm-and-up">
-      <v-list-group v-model="showActions">
-        <v-touch v-on:press="callbacks.select(todo)" slot="item">
-          <v-list-tile :name="todo._id" @click.stop.prevent="click" @contextmenu.stop.prevent="" :ripple="false">
-            <v-list-tile-action>
-              <v-checkbox
-                primary
-                hide-details
-                :input-value="todo.done"
-                @click.native.stop="callbacks.update(todo)"
-              >
-              </v-checkbox>
-            </v-list-tile-action>
-            <v-list-tile-content>
-              <v-list-tile-title v-html="todo.name"></v-list-tile-title>
-              <v-list-tile-sub-title>
-                <v-icon v-show="todo.dueDate">event</v-icon>
-                <span v-show="todo.dueDate">{{ todo.dueDate }}</span>
-                <v-icon v-show="todo.dueTime">alarm</v-icon>
-                <span v-show="todo.dueTime">{{ todo.dueTime }}</span>
-              </v-list-tile-sub-title>
-            </v-list-tile-content>
-            <v-list-tile-action>
-              <v-icon>keyboard_arrow_down</v-icon>
-            </v-list-tile-action>
+      <v-touch v-on:press="callbacks.select(todo)">
+        <v-list-group v-model="showActions">
+          <v-touch v-on:tap="click" slot="item">
+            <v-list-tile :name="todo._id" v-on:tap="click" @contextmenu.stop.prevent="null" :ripple="false">
+              <v-list-tile-action>
+                <v-checkbox
+                  primary
+                  hide-details
+                  :input-value="todo.done"
+                  @click.native.stop="callbacks.update(todo)"
+                >
+                </v-checkbox>
+              </v-list-tile-action>
+              <v-list-tile-content>
+                <v-list-tile-title v-html="todo.name"></v-list-tile-title>
+                <v-list-tile-sub-title>
+                  <v-icon v-show="todo.dueDate">event</v-icon>
+                  <span v-show="todo.dueDate">{{ todo.dueDate }}</span>
+                  <v-icon v-show="todo.dueTime">alarm</v-icon>
+                  <span v-show="todo.dueTime">{{ todo.dueTime }}</span>
+                </v-list-tile-sub-title>
+              </v-list-tile-content>
+              <v-list-tile-action>
+                <v-icon>keyboard_arrow_down</v-icon>
+              </v-list-tile-action>
+            </v-list-tile>
+          </v-touch>
+          <v-list-tile @click="">
+            <v-btn
+              flat
+              class="xs1 sm1"
+              icon
+              @click.native="callbacks.delete_(todo)"
+            >
+              <v-icon>delete</v-icon>
+            </v-btn>
+            <v-btn
+              flat
+              icon
+              class="xs1 sm1"
+              @click.native="callbacks.edit(todo)"
+            >
+              <v-icon>create</v-icon>
+            </v-btn>
           </v-list-tile>
-        </v-touch>
-        <v-list-tile @click="">
-          <v-btn
-            flat
-            class="xs1 sm1"
-            icon
-            @click.native="callbacks.delete_(todo)"
-          >
-            <v-icon>delete</v-icon>
-          </v-btn>
-          <v-btn
-            flat
-            icon
-            class="xs1 sm1"
-            @click.native="callbacks.edit(todo)"
-          >
-            <v-icon>create</v-icon>
-          </v-btn>
-        </v-list-tile>
-      </v-list-group>
+        </v-list-group>
+      </v-touch>
     </div>
     <div class="hidden-xs-only">
       <v-touch v-on:press="callbacks.select(todo)">
@@ -120,7 +122,7 @@ export default {
     },
     click: function() {
       this.showActions = !this.showActions
-    }
+    },
   },
   computed: {
   }
