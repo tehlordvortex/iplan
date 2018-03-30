f71358d2ae829b53985ff603aef788e22c84eaf3
tehlordvortex <tehlordvortex@outlook.com>
2018-03-30 12:28:09 +0100

    remove pre-existing files

--- src/helpers.js
+++
@@ -1,5 +0,0 @@
-export function deleteFromArray (array, item) {
-    if (array.indexOf(item) < 0) return array;
-    return array.slice(0, array.indexOf(item)).concat(array.slice(array.indexOf(item) + 1));
-  }
-export var debug = true;
