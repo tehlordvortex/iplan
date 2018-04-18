f052cb05827acbb08b903384e8aabac9bf01b242
tehlordvortex <tehlordvortex@outlook.com>
2018-04-18 18:29:06 +0100

    add content loading animation

---
+++ src/components/VclTodoItem.vue
@@ -0,0 +1,20 @@
+<template>
+  <vue-content-loading :width="300" :height="15">
+    <circle cx="15" cy="5" r="5" />
+    <rect x="25" y="0" rx="4" ry="4" width="100" height="10" />
+  </vue-content-loading>
+</template>
+
+<script>
+import VueContentLoading from 'vue-content-loading'
+
+export default {
+  components: {
+    VueContentLoading
+  }
+}
+</script>
+
+<style>
+
+</style>
